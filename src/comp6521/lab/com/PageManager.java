f6d85f0f517fd22b934703fac4ffeccecee6fe35
dimitri.tiago <dimitri.tiago@2fc72c54-a7dd-11de-ac05-951101a8bf3e>
2009-09-27 22:04:28 +0000

    COMP6521Lab: PageManager.java:
    Created project structure.

---
+++ src/comp6521/lab/com/PageManager.java
@@ -0,0 +1,15 @@
+/**
+ * September 27, 2009
+ * COMP6521 Lab
+ * PageManager.java: class that retrieves a page of records from a relation data text file.
+ */
+package comp6521.lab.com;
+
+/**
+ * @author dimitri.tiago
+ *
+ */
+public class PageManager 
+{
+
+}

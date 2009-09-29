fac6246f808e7968001dac8a67a3e182c583a295
julien.lheureux <julien.lheureux@2fc72c54-a7dd-11de-ac05-951101a8bf3e>
2009-09-29 00:54:27 +0000

    Simple singleton interface

--- src/comp6521/lab/com/MemoryManager.java
+++ src/comp6521/lab/com/MemoryManager.java
@@ -7,5 +7,20 @@ package comp6521.lab.com;
  */
 public class MemoryManager 
 {
-
+	// Singleton
+	private static MemoryManager ms_Instance;
+	
+	// Singleton enforcing
+	private MemoryManager(){}
+	public Object clone() throws CloneNotSupportedException { throw new CloneNotSupportedException(); }
+	public static synchronized MemoryManager getInstance()
+	{
+		if( ms_Instance == null )
+			ms_Instance = new MemoryManager();
+		
+		return ms_Instance;
+	}
+	
+	// Memory manager interface to the page manager
+	//...
 }

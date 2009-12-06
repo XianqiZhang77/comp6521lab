e8414af31bdd39560c50f2eea93917c91729f99a
dimitri.tiago <dimitri.tiago@2fc72c54-a7dd-11de-ac05-951101a8bf3e>
2009-12-06 03:39:10 +0000

    Incomplete - Query_D_Indexed: In progress.

--- src/comp6521/lab/com/QueryD_Indexed.java
+++ src/comp6521/lab/com/QueryD_Indexed.java
@@ -40,25 +40,35 @@ public class QueryD_Indexed
 		// *** build indexes ***
 		// r_name in RegionTable      [b-tree]
 		BPlusTree< RegionPage, StringRecordElement > RegionNameIndex = new BPlusTree< RegionPage, StringRecordElement >();
-		RegionNameIndex.CreateBPlusTree( RegionPage.class, StringRecordElement.class, 50, "Region.txt", "regionBTreeIndex.idx", "r_name");
+		RegionNameIndex.CreateBPlusTree( RegionPage.class, StringRecordElement.class, 50, "Region.txt", "r_nameBTreeIndex.txt", "r_name" );
 		
 		// n_regionKey in Nation      [b-tree]
 		BPlusTree< NationPage, IntegerRecordElement > NationFKIndex = new BPlusTree< NationPage, IntegerRecordElement >();
-		NationFKIndex.CreateBPlusTree( NationPage.class, IntegerRecordElement.class, "Nation.txt", "Nation_FK_tree.txt", "n_regionKey");
+		NationFKIndex.CreateBPlusTree( NationPage.class, IntegerRecordElement.class, "Nation.txt", "Nation_FK_tree.txt", "n_regionKey" );
 
+		// n_nationKey in Nation    [b-tree]
+		BPlusTree< NationPage, IntegerRecordElement > NationPKIndex = new BPlusTree< NationPage, IntegerRecordElement >();
+		NationPKIndex.CreateBPlusTree( NationPage.class, IntegerRecordElement.class, "Nation.txt", "Nation_PK_tree.txt", "n_nationKey" );
+		
 		// s_nationKey in Supplier    [b-tree]
 		BPlusTree< SupplierPage, IntegerRecordElement > SupplierFKIndex = new BPlusTree< SupplierPage, IntegerRecordElement >();
-		SupplierFKIndex.CreateBPlusTree( SupplierPage.class, IntegerRecordElement.class, "Supplier.txt", "Supplier_FK_tree.txt", "s_nationKey");
-
+		SupplierFKIndex.CreateBPlusTree( SupplierPage.class, IntegerRecordElement.class, "Supplier.txt", "Supplier_FK_tree.txt", "s_nationKey" );
+		
 		// *** process query ***
 		
-		StringRecordElement searchKey = new StringRecordElement(15);	// initialise search key
+		StringRecordElement searchKey = new StringRecordElement(50);	// initialise search key
 		searchKey.setString(r_name);
 		
 		int[] regions = RegionNameIndex.Get(searchKey);	// lookup region record numbers
 		Arrays.sort(regions);	// sort array using quick-sort
-																										// record number to key processing function
-		RecordNumberToKeyPF<RegionPage> recNumToKeyPF = new RecordNumberToKeyPF<RegionPage> (regions, RegionPage.class, "n_name", "regionKeys.tmp");
-		DB.ProcessingLoop(recNumToKeyPF);	// process region keys
+		
+		RegionToNationPF regionToNationPF = new RegionToNationPF(regions, NationFKIndex, "r_regionKey");	// get nation record #s for regions
+		int[] nations = DB.ProcessingLoop(regionToNationPF);
+		Arrays.sort(nations);	// sort nations
+
+		NationToSupplierPF nationToSuppPF = new NationToSupplierPF(nations, SupplierFKIndex, "n_nationKey");	// get supplier record #s for nations
+		int[] suppliers = DB.ProcessingLoop(nationToSuppPF);
+		
+		System.out.println("TEST");
 	}			
 }

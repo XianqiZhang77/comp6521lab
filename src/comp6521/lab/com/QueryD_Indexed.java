3ba8b3d27af1a182dee6890f35fb3815217df041
dimitri.tiago <dimitri.tiago@2fc72c54-a7dd-11de-ac05-951101a8bf3e>
2009-12-06 23:22:31 +0000

    Incomplete - Query_D_Indexed: Outputs all records from join. BUGS: non-full pages leaves spaces in output. Memory pages not correct for some reason.

--- src/comp6521/lab/com/QueryD_Indexed.java
+++ src/comp6521/lab/com/QueryD_Indexed.java
@@ -7,21 +7,15 @@ import java.util.Arrays;
 
 import comp6521.lab.com.Pages.NationPage;
 import comp6521.lab.com.Pages.RegionPage;
-import comp6521.lab.com.Pages.RegionSubsetPage;
 import comp6521.lab.com.Pages.SupplierPage;
-import comp6521.lab.com.Records.NationRecord;
-import comp6521.lab.com.Records.RegionRecord;
-import comp6521.lab.com.Records.RegionSubsetRecord;
-import comp6521.lab.com.Records.SupplierRecord;
 
-import comp6521.lab.com.Records.FloatRecordElement;
+import comp6521.lab.com.Records.Record;
+import comp6521.lab.com.Records.DateRecordElement;
 import comp6521.lab.com.Records.IntegerRecordElement;
 import comp6521.lab.com.Records.StringRecordElement;
 
 import comp6521.lab.com.Util.DB;
 import comp6521.lab.com.Util.ProcessingFunction;
-import comp6521.lab.com.Util.RecordNumberToKeyPF;
-import comp6521.lab.com.Util.key_page;
 
 /**
  * @author dimitri.tiago
@@ -37,6 +31,10 @@ public class QueryD_Indexed
 	// execute query:
 	public void ProcessQuery(String r_name)
 	{
+		// *** initialise memory manager ***
+		MemoryManager.getInstance().AddPageType( QDNationSubsetPage.class, "qd_ns_i.txt");	// add page type
+		MemoryManager.getInstance().AddPageType( QD_Page.class, "d.idx.out");	
+		
 		// *** build indexes ***
 		// r_name in RegionTable      [b-tree]
 		BPlusTree< RegionPage, StringRecordElement > RegionNameIndex = new BPlusTree< RegionPage, StringRecordElement >();
@@ -66,9 +64,106 @@ public class QueryD_Indexed
 		int[] nations = DB.ProcessingLoop(regionToNationPF);
 		Arrays.sort(nations);	// sort nations
 
-		NationToSupplierPF nationToSuppPF = new NationToSupplierPF(nations, SupplierFKIndex, "n_nationKey");	// get supplier record #s for nations
-		int[] suppliers = DB.ProcessingLoop(nationToSuppPF);
+		// *** join *** and output
+		NationSubsetPF nationRecordsPF = new NationSubsetPF(nations, SupplierFKIndex);
+		DB.ProcessingLoop(nationRecordsPF);
 		
 		System.out.println("TEST");
 	}			
+}
+
+// get the nation records indexed
+class NationSubsetPF extends ProcessingFunction<NationPage, DateRecordElement>
+{
+	// fields
+	int recCount = 0;											// count of records processed
+	int[] recordNumbers;											// array of record numbers
+	
+	QDNationSubsetPage page;									// nation subset page
+	BPlusTree< SupplierPage, IntegerRecordElement > suppIdx;	// supplier b+ tree index
+	
+	// constructor
+	public NationSubsetPF( int[] input, BPlusTree< SupplierPage, IntegerRecordElement > suppIdx )
+	{
+		// initialise
+		super( input, NationPage.class );	
+		this.suppIdx = suppIdx;				 
+		this.recordNumbers = input;			
+	}
+	
+	// start process
+	public void ProcessStart()
+	{
+		// no-op
+	}
+	
+	// process method
+	public void Process( Record r )
+	{
+		QDNationSubsetRecord ns = new QDNationSubsetRecord();	// populate nation record
+		ns.get("n_nationKey").set( r.get("n_nationKey") );
+		ns.get("n_name").set( r.get("n_name") );
+			
+		int[] nation = {recordNumbers[recCount]};	// current nation record number
+		recCount++;									// increment record count
+		
+		NationToSupplierPF nationToSuppPF = new NationToSupplierPF(nation, suppIdx, "n_nationKey");	// get supplier record #s for suppliers
+		int[] suppliers = DB.ProcessingLoop(nationToSuppPF);
+		Arrays.sort(suppliers);	// sort suppliers	
+		
+		QueryDIdxOutputPF output = new QueryDIdxOutputPF(suppliers, ns);	// output nation-supplier join
+		DB.ProcessingLoop(output);
+	}
+	
+	// end process
+	public int[] EndProcess()
+	{
+		// no-op
+		return null;
+	}
+}
+
+//get the nation records indexed
+class QueryDIdxOutputPF extends ProcessingFunction<SupplierPage, DateRecordElement>
+{
+	// fields
+	QDNationSubsetRecord	nationSubsetRec;	// nation record to join
+	QD_Page	page;								// output page
+	
+	// constructor
+	public QueryDIdxOutputPF(int[] input, QDNationSubsetRecord qDNationSubsetRecord)
+	{
+		// initialise
+		super( input, SupplierPage.class );	
+		this.nationSubsetRec = qDNationSubsetRecord;
+	}
+	
+	// start process
+	public void ProcessStart()
+	{
+		// get empty page
+		page = MemoryManager.getInstance().getEmptyPage(QD_Page.class, "d.idx.out");
+	}
+	
+	// process method
+	public void Process( Record r )
+	{
+		QD_Record qDOutput = new QD_Record();	// populate query d output record
+		qDOutput.get("s_acctBal").set( r.get("s_acctBal") );
+		qDOutput.get("s_name").set( r.get("s_name") );
+		qDOutput.get("n_name").set( nationSubsetRec.get("n_name") );
+		qDOutput.get("s_address").set( r.get("s_address") );
+		qDOutput.get("s_phone").set( r.get("s_phone") );
+		qDOutput.get("s_comment").set( r.get("s_comment") );
+			
+		page.AddRecord(qDOutput); // add to page
+	}
+	
+	// end process
+	public int[] EndProcess()
+	{
+		// free page
+		MemoryManager.getInstance().freePage(page);
+		return null;
+	}
 }
